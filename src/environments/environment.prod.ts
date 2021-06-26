5aacc4c1f405b12a3a7a66ac4a1f652073f080d5
edwinflz <edwin.95.florez@gmail.com>
2021-06-26 12:23:18 -0500

    Feature: Preparando para prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,20 @@
+const ASSETS = 'assets/data';
+const URL = 'http://localhost:5000';
+const URL_POKEMON = 'https://pokeapi.co/api/v2/pokemon';
 export const environment = {
   production: true,
   defaultLocale: 'es',
   missingKey: false,
+  urlAssets: {
+    project: `${ASSETS}/projects.json`
+  },
+  urlApi: {
+    registerUser: `${URL}/api/auth/register`,
+    loginUser: `${URL}/api/auth/login`,
+    fetchUser: `${URL}/api/users/{userId}`,
+  },
+  pokeApi: {
+    paging: `${URL_POKEMON}/?offset={offset}&limit={limit}`,
+    pokemon: `${URL_POKEMON}/{pokeId}`,
+  }
 };
